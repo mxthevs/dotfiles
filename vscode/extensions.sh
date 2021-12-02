02765f0677abe67e290569254dcc78ba90a253de
Matheus Henrique <matheuscdasilva2@hotmail.com>
2021-12-02 17:13:32 -0300

    Add VSCode extensions

---
+++ vscode/extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -xe
+
+code --install-extension chenglou92.rescript-vscode
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension eserozvataf.one-dark-pro-monokai-darker
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension naumovs.color-highlight
+code --install-extension ocamllabs.ocaml-platform
+code --install-extension PKief.material-icon-theme
+code --install-extension rocketseat.rocketseatreactjs
+code --install-extension fftp.ftp-kr
