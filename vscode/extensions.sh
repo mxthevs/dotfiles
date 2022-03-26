94d3d6ca4f7d7a0480df3cf8d7b2b26c745e10f4
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-03-26 18:01:49 -0300

    chore: Update VS Code settings

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -6,14 +6,11 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
-code --install-extension eserozvataf.one-dark-pro-monokai-darker
-code --install-extension gitkraken.gitkraken-authentication
-code --install-extension icrawl.discord-vscode
 code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension mxthevs.one-dark-pro-monokai-darker-reloaded
 code --install-extension naumovs.color-highlight
 code --install-extension ocamllabs.ocaml-platform
 code --install-extension PKief.material-icon-theme
 code --install-extension rocketseat.rocketseatreactjs
-code --install-extension fftp.ftp-kr
-code --install-extension rust-lang.rust
-code --install-extension sainnhe.gruvbox-material
+code --install-extension ruakr.ftp-kr
+code --install-extension usernamehw.errorlens
