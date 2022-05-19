ecca9f3961f7ba4494f3fb8e252b6176c2f7f585
Matheus Henrique <matheuscdasilva2@hotmail.com>
2022-05-19 17:58:29 -0300

    Update VS Code settings

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -4,7 +4,6 @@ code --install-extension chenglou92.rescript-vscode
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
-code --install-extension esbenp.prettier-vscode
 code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension mxthevs.one-dark-pro-monokai-darker-reloaded
 code --install-extension naumovs.color-highlight
